9b075487a526a6aa07fdb8d9c45edb4112ab927f
Damilola Olaniyan <damilolaniyan20@gmail.com>
2022-02-21 10:03:44 +0100

    Responsiveness

--- src/pages/marketing.js
+++ src/pages/marketing.js
@@ -28,8 +28,8 @@ export default function Marketing() {
           </p>
         </div>
 
-        <div className="flex mt-6 w-full">
-          <div className="w-3/12 pr-2">
+        <div className="flex mt-6 w-full lg:flex-row flex-col">
+          <div className="lg:w-3/12 w-full pr-2">
             <p className="text-sm mb-2 font-semibold">Get more traffic</p>
             <p className="text-gray-600 text-xs">
               Start an advertising campaign to reach your target audience and
@@ -37,7 +37,7 @@ export default function Marketing() {
             </p>
           </div>
 
-          <div className="flex flex-col w-9/12 space-y-4 mt-2">
+          <div className="flex flex-col lg:w-9/12 w-full space-y-4 mt-2">
             <div className="flex py-2 px-4 border border-solid rounded-md items-center bg-white w-full justify-center shadow">
               <span className="w-1/12 flex justify-center">
                 <FcGoogle size={24} />
@@ -84,8 +84,8 @@ export default function Marketing() {
           </div>
         </div>
 
-        <div className="flex mt-6 w-full">
-          <div className="w-3/12 pr-2">
+        <div className="flex mt-6 w-full lg:flex-row flex-col">
+          <div className="lg:w-3/12 pr-2">
             <p className="text-sm font-semibold mb-2">Make customers loyal</p>
             <p className="text-gray-600 text-xs">
               Engage past customers to build loyalty and have them return as
@@ -93,7 +93,7 @@ export default function Marketing() {
             </p>
           </div>
 
-          <div className="flex flex-col w-9/12 space-y-4 mt-2">
+          <div className="flex flex-col lg:first-letter:w-9/12 space-y-4 mt-2">
             <div className="flex py-2 px-4 border border-solid rounded-md items-center bg-white w-full justify-center shadow">
               <span className="w-1/12 flex justify-center">
                 <RiMailUnreadLine size={24} color={"#FBBF24"} />
