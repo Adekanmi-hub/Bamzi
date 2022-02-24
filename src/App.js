65107b585a68b66efe7e2332da5ac822aba55383
lafresh2664 <joshuasalifu1@gmail.com>
2022-02-24 09:48:36 +0100

    sellersDashboard

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import React from "react"
 import ProductDetails from "./pages/productDetails"
 import SellersBoard from "./pages/sellersBoard"
 import SellersStore from "./pages/sellersStore"
+import Sellers from "./pages/Sellers"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-          <Route path="/" element={<ProductDetails />} />
-          <Route path="product-details" element={<ProductDetails />} />
-          <Route path="sellers-board" element={<SellersBoard />} />
-          <Route path="sellers-store" element={<SellersStore />} />
+        <Route path="/" element={<ProductDetails />} />
+        <Route path="product-details" element={<ProductDetails />} />
+        <Route path="sellers-board" element={<SellersBoard />} />
+        <Route path="sellers-store" element={<SellersStore />} />
+        <Route path="sellers" element={<Sellers />} />
       </Routes>
     </BrowserRouter>
   )
